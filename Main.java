6edbc042b2ce21242488b42d0ba4729351ccc3d5
Moneyman2222 <158236375+Moneyman2222@users.noreply.github.com>
2024-09-14 18:28:45 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter nine numbers seperated by spaces: ");
+String s1 = input.next();
+String s2 = input.next();
+String s3 = input.next();
+String s4 = input.next();
+String s5 = input.next();
+String s6 = input.next();
+String s7 = input.next();
+String s8 = input.next();
+String s9 = input.next();
+System.out.print("Enter nine more numbers seperated by spaces: ");
+String ss1 = input.next();
+String ss2 = input.next();
+String ss3 = input.next();
+String ss4 = input.next();
+String ss5 = input.next();
+String ss6 = input.next();
+String ss7 = input.next();
+String ss8 = input.next();
+String ss9 = input.next();
+if (s1.equals(ss1)) { } else { System.err.println("the two Arrays don't match");} 
+if (s2.equals(ss2)) { } else { System.err.println("the two Arrays don't match");}  
+if (s3.equals(ss3)) { } else { System.err.println("the two Arrays don't match");} 
+if (s4.equals(ss4)) { } else { System.err.println("the two Arrays don't match");} 
+if (s5.equals(ss5)) { } else { System.err.println("the two Arrays don't match");}  
+if (s6.equals(ss6)) { } else { System.err.println("the two Arrays don't match");} 
+if (s7.equals(ss7)) { } else { System.err.println("the two Arrays don't match");}  
+if (s8.equals(ss8)) { } else { System.err.println("the two Arrays don't match");} 
+if (s9.equals(ss9)) { System.out.println("The two Arrays match"); } else { System.err.println("the two Arrays don't match"); } 
+  }
+}
+                                  
